5ad2d07f19db7537d42e3fbea220cca4f6981a2f
Joachim Breitner <mail@joachim-breitner.de>
2022-12-24 11:55:37 +0100

    Log in to fetch flights

--- fetch.sh
+++ fetch.sh
@@ -2,9 +2,48 @@
 
 set -e
 
+echo "Loggin in"
+
+if [ -z "$DHV_PASSWORD" ]
+then
+   echo "Missing environment variable \$DHV_PASSWORD"
+   exit 1
+fi
+
+if [ -z "$DHV_USERNAME" ]
+then
+   echo "Missing environment variable \$DHV_USERNAME"
+   exit 1
+fi
+
+
+rm -f cookies.txt
+rm -f status.json
+wget \
+  --save-cookies cookies.txt \
+  --keep-session-cookies \
+  https://de.dhv-xc.de/api/xc/login/status\
+  -O status.json
+
+token=$(jq -r .meta.token < status.json)
+echo "Token: $token"
+rm -f status.json
+
+wget \
+  --save-cookies cookies.txt \
+  --load-cookies cookies.txt \
+  --keep-session-cookies \
+  --post-data "uid=$DHV_USERNAME&pwd=$DHV_PASSWORD&dhvfetch=0&stay=0" \
+  --header "X-Csrf-Token: $token" \
+  -O - \
+  https://de.dhv-xc.de/api/xc/login/login
+
 if [ ! -e schauinsland2022/flights.json ]; then
   echo "flights.json: fetching"
-  wget 'https://de.dhv-xc.de/api/fli/flights?y=2022&l-y=2022&fkto%5B%5D=9306&l-fkto%5B%5D=Schauinsland%20(DE)&navpars=%7B%22start%22%3A0%2C%22limit%22%3A2000%2C%22sort%22%3A%5B%7B%22field%22%3A%22FlightDuration%22%2C%22dir%22%3A-1%7D%2C%7B%22field%22%3A%22FlightDate%22%2C%22dir%22%3A-1%7D%5D%7D' -O flights.json
+  wget \
+	--load-cookies cookies.txt \
+    'https://de.dhv-xc.de/api/fli/flights?y=2022&l-y=2022&fkto%5B%5D=9306&l-fkto%5B%5D=Schauinsland%20(DE)&navpars=%7B%22start%22%3A0%2C%22limit%22%3A2000%2C%22sort%22%3A%5B%7B%22field%22%3A%22FlightDuration%22%2C%22dir%22%3A-1%7D%2C%7B%22field%22%3A%22FlightDate%22%2C%22dir%22%3A-1%7D%5D%7D' \
+	-O flights.json
 fi
 
 for id in $(jq -r '.data[]["IDFlight"]' < schauinsland2022/flights.json); do
@@ -13,7 +52,8 @@ if [ ! -e "schauinsland2022/$id.igc.gz" ]; then
   echo "$id: fetching"
   wget \
 	--header 'Accept: application/x-igc' \
-    --header 'Cookie: xclogin=71687891d25ab74168ddbe45ac2a1ab17fe8e2fa20cb1065489373bc547fb00e; xcsid=d40963b9be6c432cb79d01534e3d4dec' "https://de.dhv-xc.de/flight/$id/igc" \
+	--load-cookies cookies.txt \
+    "https://de.dhv-xc.de/flight/$id/igc" \
 	-O "schauinsland2022/$id.igc"
 	gzip -9 "schauinsland2022/$id.igc"
 fi
@@ -23,5 +63,7 @@ if [ ! -e "schauinsland2022/$id.stats.json" ]; then
   ./flightstats.py -i "schauinsland2022/$id.igc.gz" > "schauinsland2022/$id.stats.json.tmp"
   mv "schauinsland2022/$id.stats.json.tmp" "schauinsland2022/$id.stats.json"
 fi
+
+rm -f cookies.txt
 
 done
