addf63baf0e750d33a3c8a38e45e0e10678bf795
Joachim Breitner <mail@joachim-breitner.de>
2022-12-24 12:36:23 +0100

    Create a github action

--- shell.nix
+++ shell.nix
@@ -5,14 +5,10 @@ stdenv.mkDerivation rec {
     gdal
     gpsbabel
     python3
-    python3Packages.gpxpy
-    python3Packages.pip
-    python3Packages.virtualenv
-    python3Packages.numpy
     python3Packages.ipython
     python3Packages.folium
-    python3Packages.shapely
-    python3Packages.geopy
+#    python3Packages.shapely
+#    python3Packages.geopy
     python3Packages.geographiclib
     python3Packages.snakeviz
     python3Packages.jinja2
