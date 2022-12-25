7bc72306748ad43697f279d34728c4864df5681c
Joachim Breitner <mail@joachim-breitner.de>
2022-12-26 00:11:15 +0100

    Nur colibri

--- fetch.sh
+++ fetch.sh
@@ -51,7 +51,7 @@ if [ ! -e schauinsland2022/flights.json ]; then
   wget \
         --no-verbose \
 	--load-cookies cookies.txt \
-    "https://de.dhv-xc.de/api/fli/flights?y=2022&fkto%5B%5D=9306&fkto%5B%5D=11362&navpars=%7B%22start%22%3A0%2C%22limit%22%3A$limit%7D" \
+    "https://de.dhv-xc.de/api/fli/flights?y=2022&fkto%5B%5D=9306&fkto%5B%5D=11362&clubde%5B%5D=130&navpars=%7B%22start%22%3A0%2C%22limit%22%3A$limit%7D" \
 	-O schauinsland2022/flights.json
 fi
 
