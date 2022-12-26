e07fe36f3fdc16a6a5e4d0e809107f4592c6f4c0
Joachim Breitner <mail@joachim-breitner.de>
2022-12-26 23:00:15 +0100

    Correct date range

--- fetch-flights.sh
+++ fetch-flights.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-# For testing
-limit=20
-year=2022
+limit=2000
+year=2023
 
 echo "flights.json: fetching"
 wget \
     --no-verbose \
 	--load-cookies _tmp/cookies.txt \
-    "https://de.dhv-xc.de/api/fli/flights?y=$year&fkto%5B%5D=9306&fkto%5B%5D=11362&clubde%5B%5D=130&navpars=%7B%22start%22%3A0%2C%22limit%22%3A$limit%7D" \
+    "https://de.dhv-xc.de/api/fli/flights?d0=1.1.$year&d1=15.9.$year&fkto%5B%5D=9306&fkto%5B%5D=11362&clubde%5B%5D=130&navpars=%7B%22start%22%3A0%2C%22limit%22%3A$limit%7D" \
 	-O _tmp/flights.json
