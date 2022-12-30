9e518b5f74c96d8cac3b04e58b6a78334f682cef
Joachim Breitner <mail@joachim-breitner.de>
2022-12-30 11:36:10 +0100

    Add opt-out code

--- fetch-flights.sh
+++ fetch-flights.sh
@@ -10,4 +10,10 @@ wget \
     --no-verbose \
 	--load-cookies _tmp/cookies.txt \
     "https://de.dhv-xc.de/api/fli/flights?d0=1.1.$year&d1=15.9.$year&fkto%5B%5D=9306&fkto%5B%5D=11362&clubde%5B%5D=130&navpars=%7B%22start%22%3A0%2C%22limit%22%3A$limit%7D" \
-	-O _tmp/flights.json
+	-O _tmp/flights.json.tmp
+echo -n "Flights before opt-out: "
+jq '.data | length' < _tmp/flights.json.tmp
+# pilot opt-out
+jq '.data |= map(select(.FKPilot != "14677"  ))' < _tmp/flights.json.tmp > _tmp/flights.json
+echo -n "Flights after opt-out: "
+jq '.data | length' < _tmp/flights.json
